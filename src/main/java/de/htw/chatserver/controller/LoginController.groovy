673a49da7713a9d9d7ee1b024ba5e1479cc210e5
Vera Peuntinger <vera@peuntinger.de>
2016-06-20 15:53:28 +0200

    LoginController zurückgesetzt

--- src/main/java/de/htw/chatserver/controller/LoginController.groovy
+++ src/main/java/de/htw/chatserver/controller/LoginController.groovy
@@ -3,8 +3,6 @@ package de.htw.chatserver.controller
 import de.htw.chatserver.persistence.User
 import de.htw.chatserver.service.UserService
 
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
 import javax.ws.rs.Consumes
 import javax.ws.rs.POST
 import javax.ws.rs.Path
@@ -13,7 +11,7 @@ import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 import javax.ws.rs.ext.Provider
-//import org.vertx.groovy.core.http.HttpServerRequest
+
 /**
  * Created by Ju on 06.06.2016.
  */
@@ -23,40 +21,19 @@ class LoginController {
 
     UserService loginService;
 
-   @POST
+    @Context
+    org.glassfish.grizzly.http.server.Request request
+
+    @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    def loginPost(User user, @Context HttpServletRequest request) {
+    def loginPost(User user) {
         println("start login mail: ${user.mail} und password: ${user.password}")
-
-//        println( request )
 
         loginService = new UserService();
         //TODO IP-ADresse auslesen
-        String remoteAddr
-        if (request == null) {
-            remoteAddr = "dummyIp" // TODO dummy-Wert muss entfernt werden sobald ip ausgelesen werden kann
-        } else {
-            remoteAddr = request.getHeader("X-Forwarded-For")
-            if (remoteAddr == null || remoteAddr.length() == 0 || "unknown".equalsIgnoreCase(remoteAddr)) {
-                remoteAddr = request.getHeader("Proxy-Client-IP")
-            }
-            if (remoteAddr == null || remoteAddr.length() == 0 || "unknown".equalsIgnoreCase(remoteAddr)) {
-                remoteAddr = request.getHeader("WL-Proxy-Client-IP")
-            }
-            if (remoteAddr == null || remoteAddr.length() == 0 || "unknown".equalsIgnoreCase(remoteAddr)) {
-                remoteAddr = request.getHeader("HTTP_CLIENT_IP")
-            }
-            if (remoteAddr == null || remoteAddr.length() == 0 || "unknown".equalsIgnoreCase(remoteAddr)) {
-                remoteAddr = request.getHeader("HTTP_X_FORWARDED_FOR")
-            }
-            if (remoteAddr == null || remoteAddr.length() == 0 || "unknown".equalsIgnoreCase(remoteAddr)) {
-                remoteAddr = request.getRemoteAddr()
-            }
-//            return remoteAddr
+        String remoteAddr = request.getRemoteAddr()
 
-//            remoteAddr = request.getRemoteAddr()
-        }
         println(remoteAddr)
 
         boolean isValidLogin = loginService.isUserAndPwdValid(user.mail, user.password, remoteAddr)
